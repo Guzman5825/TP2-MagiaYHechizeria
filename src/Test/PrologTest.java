efd5baeb5639f364e963135ecf8512a1260ce2ea
Nicolás <nicolas20032401@gmail.com>
2024-11-09 16:29:06 -0300

    test basico para prolog

---
+++ src/Test/PrologTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Unidades.Estudiante;
+import prolog.ManejoProlog;
+
+class PrologTest {
+	
+	@Test
+	void testPuedeLanzarConSuficienteMana() {
+		Estudiante harry = new Estudiante("Harry");
+		harry.setMana(100);
+		assertTrue(ManejoProlog.puedeLanzarHechizo("septusembra", harry.getMana()));
+	}
+	
+	
+	@Test
+	void testPuedeLanzarInsuficienteMana() {
+		Estudiante harry = new Estudiante("Harry");
+		harry.setMana(10);
+		assertFalse(ManejoProlog.puedeLanzarHechizo("septusembra", harry.getMana()));
+	}
+
+}
