9af4b4cdc7da649678cfc8022b3a1af23687a254
Nicolás <nicolas20032401@gmail.com>
2024-11-09 16:21:26 -0300

    embellecimiento de salidas en pantalla

--- src/app/App.java
+++ src/app/App.java
@@ -5,11 +5,11 @@ public class App {
 	public static void main(String[] args) {
 		Juego juego=new Juego();
 		
-		System.out.println("carga de batallones");
+		System.out.println("-----Carga de batallones-----");
 		juego.cargarDatos();
-		System.out.println("\n\n iniciandop la batalla");
+		System.out.println("\n\n-----Iniciando la batalla-----");
 		juego.jugar();
-		System.out.println("juegoTerminado");
+		System.out.println("-----Juego Terminado-----");
 	}
 
 }
