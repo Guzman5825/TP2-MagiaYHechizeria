4602d47c14910eaca73e7fc5c9b3a2368cc22752
Nicolás <nicolas20032401@gmail.com>
2024-11-10 22:54:19 -0300

    Eliminacion comentarios y dead code

--- src/Hechizos/Incendium.java
+++ src/Hechizos/Incendium.java
@@ -20,13 +20,6 @@ public class Incendium extends HechizoBase {
 		this.turnos = 5;
 		this.descripcion = "Hechizo de area fuego, solo lo pueden utilizar los estudiantes.";
 	}
-	/*
-	 * Incendium(Personaje lanzador, List<Personaje> objetivos, int dañoBase, int
-	 * costeMana, int turnos){ this.nombre = "Incendium"; this.lanzador = lanzador;
-	 * this.objetivo = objetivos; this.dañoBase = dañoBase; this.costeMana =
-	 * costeMana; this.turnos = turnos; this.descripcion =
-	 * "Hechizo de area fuego, solo lo pueden utilizar los estudiantes."; }
-	 */
 
 	@Override
 	public void ejecutar() {
